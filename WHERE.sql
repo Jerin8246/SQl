1fee29677176fe79038253aa21975f10afa54f22
Jerin8246 <154041891+Jerin8246@users.noreply.github.com>
2025-04-06 13:47:33 -0700

    Practice WHERE Clause and logical operators

---
+++ WHERE.sql
@@ -0,0 +1,91 @@
+SELECT *
+FROM employee_salary;
+
+SELECT *
+FROM employee_salary
+WHERE first_name = 'Leslie';
+
+SELECT * 
+FROM employee_salary
+WHERE salary > 50000;
+
+SELECT * 
+FROM employee_salary
+WHERE salary <= 50000;
+
+
+SELECT *
+FROM employee_demographics
+WHERE gender = 'FEMALE';
+
+SELECT *
+FROM employee_demographics
+WHERE gender != 'FEMALE';
+
+SELECT *
+FROM employee_demographics
+WHERE birth_date > '1985-01-01';
+
+
+-- AND OR NOT -- Logical Operators
+
+SELECT *
+FROM employee_demographics
+WHERE birth_date > '1985-01-01'
+AND gender = 'MALE';
+
+SELECT *
+FROM employee_demographics
+WHERE birth_date > '1985-01-01'
+OR gender = 'MALE';
+
+SELECT *
+FROM employee_demographics
+WHERE birth_date > '1985-01-01'
+AND NOT gender = 'FEMALE';
+
+SELECT *
+FROM employee_demographics
+WHERE (first_name = 'Leslie' AND age = 44) OR age > 55;
+
+
+SELECT * 
+FROM employee_demographics 
+WHERE first_name = 'Jerry';
+
+-- LIKE Statement
+-- % and  _
+-- % means anything
+#  - underscore means something specific
+
+SELECT * 
+FROM employee_demographics 
+WHERE first_name LIKE 'Jer%';
+
+SELECT * 
+FROM employee_demographics 
+WHERE first_name LIKE '%er%';
+
+SELECT * 
+FROM employee_demographics
+WHERE first_name LIKE 'a%';
+
+SELECT *
+FROM employee_demographics
+WHERE first_name LIKE 'a__';
+
+SELECT *
+FROM employee_demographics
+WHERE first_name LIKE 'a___';
+
+SELECT *
+FROM employee_demographics
+WHERE first_name LIKE 'a___%';
+
+
+SELECT *
+FROM employee_demographics
+WHERE first_name LIKE '1989%';
+
+
+
