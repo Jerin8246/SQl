c650e49fc13e067f8bb3da58b14e38c64fb157f7
Jerin8246 <154041891+Jerin8246@users.noreply.github.com>
2025-04-07 14:25:27 -0700

    Practice HAVING

---
+++ HAVING.sql
@@ -0,0 +1,28 @@
+-- HAVING vs WHERE
+
+SELECT gender, AVG(age)
+FROM employee_demographics
+GROUP BY  gender;
+
+# HAVING COMES right  after group By
+SELECT gender, AVG(age)
+FROM employee_demographics
+group by gender
+HAVING AVG(age) >  40;
+
+SELECT OCCUPATION, AVG(salary)
+FROM employee_salary
+GROUP BY  occupation;
+
+# Where is used before GROUPBY and HAVING is used after GROUPBY
+
+SELECT occupation, AVG(salary)
+FROM employee_salary
+WHERE  occupation LIKE '%manager%'    # filtering in the row level
+GROUP BY occupation;
+
+SELECT occupation, AVG(salary)
+FROM employee_salary
+WHERE  occupation LIKE '%manager%'    # filtering out the row level
+GROUP BY occupation
+HAVING AVG(salary) > 75000;   #  filtering out in the aggregated function level. Agregated functions will be only create after GROUP By. THis is the reason it comes after GROUP BY.
